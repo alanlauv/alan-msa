0ae585d8fa0c6fd1e2a989634ed28ca7aa1661ac
Alan <alau645@aucklanduni.ac.nz>
2015-11-24 15:11:11 +1300

    added models and controllers

--- Alan Nandoso/Alan Nandoso/Models/Alan_NandosoContext.cs
+++ Alan Nandoso/Alan Nandoso/Models/Alan_NandosoContext.cs
@@ -35,5 +35,30 @@ namespace Alan_Nandoso.Models
                 this.AutomaticMigrationDataLossAllowed = true;
             }
         }
+
+        protected override void Seed(Alan_NandosoContext context)
+        {
+            var students = new List<Student>
+            {
+                new Student { FirstMidName = "Carson",   LastName = "Alexander",
+                    EnrollmentDate = DateTime.Parse("2010-09-01") },
+                new Student { FirstMidName = "Meredith", LastName = "Alonso",
+                    EnrollmentDate = DateTime.Parse("2012-09-01") },
+                new Student { FirstMidName = "Arturo",   LastName = "Anand",
+                    EnrollmentDate = DateTime.Parse("2013-09-01") },
+                new Student { FirstMidName = "Gytis",    LastName = "Barzdukas",
+                    EnrollmentDate = DateTime.Parse("2012-09-01") },
+                new Student { FirstMidName = "Yan",      LastName = "Li",
+                    EnrollmentDate = DateTime.Parse("2012-09-01") },
+                new Student { FirstMidName = "Peggy",    LastName = "Justice",
+                    EnrollmentDate = DateTime.Parse("2011-09-01") },
+                new Student { FirstMidName = "Laura",    LastName = "Norman",
+                    EnrollmentDate = DateTime.Parse("2013-09-01") },
+                new Student { FirstMidName = "Nino",     LastName = "Olivetto",
+                    EnrollmentDate = DateTime.Parse("2005-08-11")}
+            };
+            students.ForEach(s => context.Students.AddOrUpdate(p => p.LastName, s));
+            context.SaveChanges();
+        }
     }
 }
--- Alan Nandoso/Alan Nandoso/Models/Comment.cs
+++ Alan Nandoso/Alan Nandoso/Models/Comment.cs
@@ -11,7 +11,7 @@ namespace Alan_Nandoso.Model
         public int ID { get; set; }
         public string Name { get; set; }
         public string Body { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
 
         [JsonIgnore]
         public virtual Reply Reply { get; set; }
