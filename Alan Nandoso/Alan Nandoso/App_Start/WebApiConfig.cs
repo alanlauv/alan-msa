f127bc7972c1f9abe16e0ef36226ae35840e4b7c
Alan <alau645@aucklanduni.ac.nz>
2015-11-24 19:52:52 +1300

    next publish (but error)

--- Alan Nandoso/Alan Nandoso/App_Start/WebApiConfig.cs
+++ Alan Nandoso/Alan Nandoso/App_Start/WebApiConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-//using System.Web.Http.Cors;
+using System.Web.Http.Cors;
 
 namespace Alan_Nandoso
 {
@@ -11,8 +11,8 @@ namespace Alan_Nandoso
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            //var cors = new EnableCorsAttribute("*", "*", "*");
-            //config.EnableCors(cors);
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
