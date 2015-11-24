b37d20d614abe0b6a1990e8f0128363115c99e07
Alan <alau645@aucklanduni.ac.nz>
2015-11-24 15:04:17 +1300

    added livechat, models and controllers

---
+++ Alan Nandoso/Alan Nandoso/Models/Alan_NandosoContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Alan_Nandoso.Models
+{
+    public class Alan_NandosoContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Alan_NandosoContext() : base("name=Alan_NandosoContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Alan_NandosoContext, MyConfiguration>());
+        }
+
+        public System.Data.Entity.DbSet<Alan_Nandoso.Model.Comment> Comments { get; set; }
+
+        public System.Data.Entity.DbSet<Alan_Nandoso.Model.Reply> Replies { get; set; }
+
+        public class MyConfiguration : DbMigrationsConfiguration<Alan_NandosoContext>
+        {
+            public MyConfiguration()
+            {
+                // enable migrations so db knows how to update based on model
+                this.AutomaticMigrationsEnabled = true;
+
+                this.AutomaticMigrationDataLossAllowed = true;
+            }
+        }
+    }
+}
---
+++ Alan Nandoso/Alan Nandoso/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alan_Nandoso.Model
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public string Date { get; set; }
+
+        [JsonIgnore]
+        public virtual Reply Reply { get; set; }
+    }
+}
