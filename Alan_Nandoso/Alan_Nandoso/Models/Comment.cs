10ff830ce2cc29f57cbee9ce27164818840a7765
Alan <alau645@aucklanduni.ac.nz>
2015-11-25 15:47:20 +1300

    fresh project with working swagger

---
+++ Alan_Nandoso/Alan_Nandoso/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Alan_Nandoso.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Reply> Replies { get; set; }
+    }
+}
