bb0be1f3aaa2f35c3c8eac2da6b434aa6e455b24
Alan <alau645@aucklanduni.ac.nz>
2015-11-27 10:35:00 +1300

    published. need to do feedback and reply

--- Alan_Nandoso/Alan_Nandoso/Scripts/ReplyModule.js
+++ Alan_Nandoso/Alan_Nandoso/Scripts/ReplyModule.js
@@ -1,15 +1,15 @@
 ﻿// We've sepearated out all the functions related to making the AJAX calls to the API
 // Just keeps things tidy, allows us to keep some things private
-var FeedbackModule = (function () {
+var ReplyModule = (function () {
 
     // Return anything that you want to expose outside the closure
     return {
-        getFeedback: function (callback) {
+        getReplies: function (callback) {
 
             $.ajax({
                 type: "GET",
                 dataType: "json",
-                url: "http://alan-nandoso.azurewebsites.net/api/Comments",
+                url: "http://alan-nandoso.azurewebsites.net/api/Replies",
                 success: function (data) {
                     console.log(data);
                     callback(data);
@@ -26,17 +26,18 @@ var SubmitFeedbackModule = (function () {
         submitFeedback: function () {
 
             var name = document.getElementById("feedbackName").value;
-            var feedback = document.getElementById("feedbackContent").value;
+            var reply = document.getElementById("feedbackContent").value;
             var date = new Date();
+            //var commentID = 
 
             console.log(Name, Feedback, date);
 
             $.ajax({
                 type: "POST",
-                data: JSON.stringify({ Name: name, Body: feedback, Date: date }),
+                data: JSON.stringify({ Name: name, Body: reply, Date: date }),
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
-                url: "http://alan-nandoso.azurewebsites.net/api/Comments",
+                url: "http://alan-nandoso.azurewebsites.net/api/Replies",
                 success: function (data) {
                     console.log(data);
                     //location.reload();
@@ -44,4 +45,6 @@ var SubmitFeedbackModule = (function () {
             });
         }
     };
+
+
 }());
